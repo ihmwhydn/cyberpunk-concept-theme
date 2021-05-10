89ba84f688f01bbc68fbf1c896af93a29615caf6
ihmwhydn <ihm245@gmail.com>
2021-05-10 15:38:59 +0700

    add source code

---
+++ js/myjs.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+      })
+
+      $(function () {
+        $('[data-toggle="popover"]').popover()
+      })
+
+
+  });
